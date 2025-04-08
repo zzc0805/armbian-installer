4a5857a63a3da390f358c5ca40c13011b9fd467a
zzc0805 <2659193818@qq.com>
2025-04-08 17:41:49 +0800

    Update istoreos.sh

--- istoreos.sh
+++ istoreos.sh
@@ -2,8 +2,8 @@
 mkdir -p openwrt
 
 REPO="wukongdaily/armbian-installer"
-TAG="2025-03-12"
-FILE_NAME="istoreos-22.03.7-2024122712-x86-64-squashfs-combined-efi.img.gz"
+TAG="2025-04-08"
+FILE_NAME="istoreos-22.03.7-2025040711-x86-64-squashfs-combined-efi.img.gz"
 OUTPUT_PATH="openwrt/istoreos.img.gz"
 DOWNLOAD_URL=$(curl -s https://api.github.com/repos/$REPO/releases/tags/$TAG | jq -r '.assets[] | select(.name == "'"$FILE_NAME"'") | .browser_download_url')
 
